ccde682f743bb6185d45f74f7cc77119159d1c4b
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-02-24 21:40:01 -0500

    taking notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
+// Components let you split the UI into independent, reusable pieces, and think about each piece in isolation
+// Fragment lets you group a list of children without adding extra nodes to the DOM
 import { connect } from 'react-redux';
+// connect function connects a react component to a redux store
 import {Switch, Route } from 'react-router-dom';
 
 
