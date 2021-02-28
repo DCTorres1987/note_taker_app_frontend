6fe1e4b759326fecdc23010d4e3e6bc5ec918f88
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-02-27 22:01:36 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowswerRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers/rootReducer.js';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <BrowswerRouter>
     <App />
-  </React.StrictMode>,
+    </BrowswerRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/notes/noteList.js
+++ src/notes/noteList.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const NoteList = ({noteList}) => {
-    return (
-        <div>
-
-        </div>
-    )
+    console.log(noteList);
 }
 
 const mapStateToProps = (state) => {
