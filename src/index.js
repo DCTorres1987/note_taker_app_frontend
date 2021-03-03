8fac666772d5e32c3adc6d7120c1ce9c1e6f8020
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-03-02 22:26:55 -0500

    issue with provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer.js';
+import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(() => [], {}, applyMiddleware())
 
 
 
